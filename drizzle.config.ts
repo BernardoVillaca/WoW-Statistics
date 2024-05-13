4042bb43b75067dafa265dd6fe1eb9ebec7dd6be
BernardoVillaca <bernardo-villa@hotmail.com>
2024-05-13 14:46:02 -0300

    linked db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,10 @@ import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
   driver: "pg",
   dbCredentials: {
-    connectionString: env.DATABASE_URL,
+    connectionString: env.POSTGRES_URL,
   },
   tablesFilter: ["wowstats_*"],
 } satisfies Config;
