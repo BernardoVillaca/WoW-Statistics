40284c6f7dcda373cec57484177c7b5e668b1362
BernardoVillaca <bernardo-villa@hotmail.com>
2024-06-12 13:10:52 -0300

    put all the cronjobs inside the cronjobs folder changed the api url calls and addded getpossible realms to the cronjobs

---
+++ src/app/api/cronJobs/getPossibleRealms/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getPossibleRealms } from '~/server/actions/getPossibleRealms';
+import { updateLeaderboard } from '~/server/actions/updateLeaderboard';
+export const maxDuration = 300;
+export const dynamic = 'force-dynamic';
+
+export async function GET(req: NextRequest) {
+    // Verify the shared secret
+    const authHeader = req.headers.get('authorization');
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+        return new Response('Unauthorized', {
+            status: 401,
+        });
+    }
+
+    try {
+
+        console.log('Getting possible realms and entry count');
+        await getPossibleRealms();
+
+        console.log('Finished getting possible realms and entry count');
+        return NextResponse.json({ message: 'Finished getting possible realms and entry count' });
+    } catch (error) {
+        console.error('Error running scheduled tasks:', error);
+        return NextResponse.json({ error: 'Error running scheduled tasks.' }, { status: 500 });
+    }
+}
