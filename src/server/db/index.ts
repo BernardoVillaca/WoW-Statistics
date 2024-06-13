352a9f0c93c010d54ec28a202745e97a2a09e18a
BernardoVillaca <bernardo-villa@hotmail.com>
2024-06-13 10:42:27 -0300

    changed db file for deployment

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,23 +1,6 @@
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { sql } from "@vercel/postgres";
 import * as schema from './schema'
-import { first } from 'node_modules/cheerio/lib/api/traversing';
-import { getExtraDataForEachPlayer } from '../actions/getExtraDataForEachPlayer';
-import { updateShuffle } from '../actions/updateShuffle';
-
-
 
 // Use this object to send drizzle queries to your DB
 export const db = drizzle(sql, { schema });
-
-let firstRun = true;
-
-if(firstRun) {
-
-
-   await getExtraDataForEachPlayer('classic', 'eu', '3v3');
-
-
-
-    firstRun = false;
-}
