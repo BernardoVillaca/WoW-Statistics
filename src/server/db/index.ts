f9b287cc7054a3b7072e065d9f353e948dbed481
BernardoVillaca <bernardo-villa@hotmail.com>
2024-06-14 00:19:28 -0300

    made updateRatingscutoff more readable

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,22 +1,6 @@
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { sql } from "@vercel/postgres";
 import * as schema from './schema'
-import { updateRatingsCutoffs } from '~/server/actions/updateRatingsCutoffs';
-import { getExtraDataForEachPlayer } from '../actions/getExtraDataForEachPlayer';
-import { updateLeaderboard } from '../actions/updateLeaderboard';
 
-
-
-
-// Use this object to send drizzle queries to your DB
 export const db = drizzle(sql, { schema });
 
-// let firstRun = true;
-
-// if (firstRun) {
-//   await updateLeaderboard('retail', 'us', '3v3');
-
-
-
-//     firstRun = false;
-// }
