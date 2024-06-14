4e5222dadd532309e4320e44b625a97d7a6a756f
BernardoVillaca <bernardo-villa@hotmail.com>
2024-06-14 14:19:58 -0300

    merged updateus and updateeu to updateRetail

--- src/app/api/cronJobs/updateDb/updateRetail/route.ts
+++ src/app/api/cronJobs/updateDb/updateRetail/route.ts
@@ -5,25 +5,26 @@ export const maxDuration = 300;
 export const dynamic = 'force-dynamic';
 
 export async function GET(req: NextRequest) {
-    console.log('US update endpoint hit');
-    
-    // Verify the shared secret
+   // Verify the shared secret
     const authHeader = req.headers.get('authorization');
     if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-        console.log('Unauthorized request to US update');
+        console.log('Unauthorized request to retail update');
         return new Response('Unauthorized', {
             status: 401,
         });
     }
 
     try {
-        console.log('Running update US tables tasks...');
+        console.log('Running update retail tables tasks...');
         await updateLeaderboard('retail', 'us', '3v3');
         await updateLeaderboard('retail', 'us', '2v2');
         await updateLeaderboard('retail', 'us', 'rbg');
+        await updateLeaderboard('retail', 'eu', '3v3');
+        await updateLeaderboard('retail', 'eu', '2v2');
+        await updateLeaderboard('retail', 'eu', 'rbg');
         
-        console.log('Finished updating US tables tasks');
-        return NextResponse.json({ message: 'Finished updating US tables tasks' });
+        console.log('Finished updating us tables tasks');
+        return NextResponse.json({ message: 'Finished updating retail tables tasks' });
     } catch (error) {
         console.error('Error running scheduled tasks:', error);
         return NextResponse.json({ error: 'Error running scheduled tasks.' }, { status: 500 });
