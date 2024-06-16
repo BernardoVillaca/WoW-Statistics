1d3c80ef39b31f61ea0bc4c31fd5e34e0a09f586
BernardoVillaca <bernardo-villa@hotmail.com>
2024-06-16 01:49:24 -0300

    deploy fix

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,15 +1,7 @@
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { sql } from "@vercel/postgres";
 import * as schema from './schema'
-import { getExtraDataForEachPlayer } from '../actions/getExtraDataForEachPlayer';
 
 export const db = drizzle(sql, { schema });
 
 
-
-// let first = true;
-
-// if(first) {
-//     await getExtraDataForEachPlayer('classic', 'us', 'rbg');
-//     first = false;
-// }
