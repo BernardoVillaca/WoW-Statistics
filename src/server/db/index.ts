184de92a5aa8af974fd64d651ff5fc8c8ea3d257
BernardoVillaca <bernardo-villa@hotmail.com>
2024-06-28 13:11:20 -0300

    trying to deploy

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,21 +1,9 @@
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { sql } from "@vercel/postgres";
 import * as schema from './schema'
-import { updateRatingsCutoffs } from '../actions/updateRatingsCutoffs';
-import { updateActivityStatistics } from '../actions/updateActivityStatistics';
-import { updateClassSpecCount } from '../actions/updateClassSpecCount';
-import { updateShuffle } from '../actions/updateShuffle';
+
 
 
 
 export const db = drizzle(sql, { schema });
 
- let firstRun = true;
-
- if(firstRun) {
-
-  // await updateShuffle('us', 'first');
- 
-
-    firstRun = false;
- }
