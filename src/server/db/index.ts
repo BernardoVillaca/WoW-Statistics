6a65664ef3894d60c801ede31afe27bc8ef8fca0
BernardoVillaca <bernardo-villa@hotmail.com>
2024-10-28 22:54:20 -0300

    removed db function

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,17 +1,7 @@
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { sql } from "@vercel/postgres";
 import * as schema from './schema'
-import { getAuthToken } from '../actions/getAuthToken';
-import { updateLeaderboard } from '../actions/updateLeaderboard';
 
 
 
 export const db = drizzle(sql, { schema });
-
-// let first = true;
-
-// if (first) {
-
-//     await updateLeaderboard('retail', 'us', '3v3')
-//     first = false;
-// }
